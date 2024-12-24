b15bdd13ccba7e01dc9eba96a23db1f79110a41a
JeanS <jean.sinche@alarcongroup.us>
2024-12-24 11:26:32 -0500

    Styles text elements with new color palette
    Applies new primary, secondary, and tertiary colors to text elements throughout the app.  This enhances visual appeal and consistency.

--- app/index.tsx
+++ app/index.tsx
@@ -8,10 +8,16 @@ const App = () => {
         <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
           Hola mundo
         </Text>
-        <Text className="text-4xl font-work-black">Hola mundo</Text>
-        <Text className="text-3xl font-work-medium">Hola mundo</Text>
-        <Text className="text-2xl font-work-light">Hola mundo</Text>
-        <Text className="text-xl">Hola mundo</Text>
+        <Text className="text-4xl font-work-black text-primary">
+          Hola mundo
+        </Text>
+        <Text className="text-3xl font-work-medium text-secondary">
+          Hola mundo
+        </Text>
+        <Text className="text-2xl font-work-light text-secondary-100">
+          Hola mundo
+        </Text>
+        <Text className="text-xl text-tertiary">Hola mundo</Text>
       </View>
     </SafeAreaView>
   );
