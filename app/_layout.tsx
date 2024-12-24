cf44e556fe6713180c8b56bc9cfb69e03880e62e
JeanS <jean.sinche@alarcongroup.us>
2024-12-24 11:16:29 -0500

    Fixes layout flash on initial load
    Delays rendering the app layout until fonts are loaded. This prevents a flash of unstyled content on initial load, improving the user experience.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Slot } from "expo-router";
+import React, { useEffect } from "react";
+import { Slot, SplashScreen } from "expo-router";
 import { useFonts } from "expo-font";
 import "./global.css";
+SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
-  useFonts({
+  const [fontsLoaded, error] = useFonts({
     "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
     "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
     "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
   });
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+  if (!fontsLoaded) return null;
   return <Slot />;
 };
 
