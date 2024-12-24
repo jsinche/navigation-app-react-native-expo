900fa83a0d7c99b6a7af056f247041f6f0ba467c
JeanS <jean.sinche@alarcongroup.us>
2024-12-24 17:39:46 -0500

    tabs

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Link, Redirect } from "expo-router";
 
 const App = () => {
-  return <Redirect href="/home" />;
+  return <Redirect href="/tabs" />;
   // return (
   //   <SafeAreaView>
   //     <View className="mt-6 mx-2.5">
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,32 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "green",
+        // tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="(stack)"
+        options={{
+          title: "Stack",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="person-add-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: "Favoritos",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="star-outline" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
