993d5064bd05ba5584f7e137de1e41ceffe529b4
JeanS <jean.sinche@alarcongroup.us>
2024-12-24 11:16:19 -0500

    Adds custom fonts and styles to the app
    Integrates Work Sans font family (Black, Light, Medium) into the application.
    Applies the new fonts to the text elements within the app's main view.
    Wraps the main view with SafeAreaView for better platform compatibility and styling consistency. Updates margin and text size.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Slot } from "expo-router";
+import { useFonts } from "expo-font";
 import "./global.css";
 const RootLayout = () => {
+  useFonts({
+    "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+    "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
+  });
   return <Slot />;
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const App = () => {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl font-bold text-blue-500">Hola mundo</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
+          Hola mundo
+        </Text>
+        <Text className="text-4xl font-work-black">Hola mundo</Text>
+        <Text className="text-3xl font-work-medium">Hola mundo</Text>
+        <Text className="text-2xl font-work-light">Hola mundo</Text>
+        <Text className="text-xl">Hola mundo</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
