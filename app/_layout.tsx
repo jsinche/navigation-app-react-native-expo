591c6f3c8f28f4b368d8f61792782130aefe0cd1
JeanS <jean.sinche@alarcongroup.us>
2024-12-24 10:48:41 -0500

    install tailwind

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Slot } from "expo-router";
+import "./global.css";
+const RootLayout = () => {
+  return <Slot />;
+};
+
+export default RootLayout;
