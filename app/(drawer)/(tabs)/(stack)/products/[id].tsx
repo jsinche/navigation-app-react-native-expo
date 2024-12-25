e702b1d2c48bb8575345b8676bfd37ae302ead72
JeanS <jean.sinche@alarcongroup.us>
2024-12-24 21:33:39 -0500

    Updates product screen title dynamically
    Sets the product screen title using the product's name retrieved from the product data.  If no product is found, defaults to "Producto".

--- app/(drawer)/(tabs)/(stack)/products/[id].tsx
+++ app/(drawer)/(tabs)/(stack)/products/[id].tsx
@@ -1,11 +1,18 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Redirect, useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
 import { products } from "@/store/products.store";
 
 const ProductScreen = () => {
   const { id } = useLocalSearchParams();
+  const navigation = useNavigation();
   const product = products.find((p) => p.id === id);
+  useEffect(() => {
+    navigation.setOptions({
+      title: product?.title ?? "Producto",
+    });
+  }, [product]);
+
   if (!product) return <Redirect href="/" />;
   return (
     <View className="px-5 mt-2">
